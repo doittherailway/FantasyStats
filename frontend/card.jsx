5955a3cbe20a879c76f04ec202296b44d7dda22f
railway <doittherailway@gmail.com>
2019-06-12 20:00:52 -0700

    Add click event to Card component

--- frontend/card.jsx
+++ frontend/card.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Card = (props) => {
+    const handleClick = () => {
+        return(
+            <div>
+                {props.playerName} Stats go here
+            </div>
+        )
+    };
+
     return(
         <li>
             <div>{props.playerName}</div>
             <div>Player Image</div>
+            <p onClick={handleClick}>Expand</p>
         </li>
     )
 }
